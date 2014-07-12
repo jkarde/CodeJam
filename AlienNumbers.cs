95b5d6bf80e6d470ccf32ef12e5daa7e90bedbda
jkarde <jkarde@tavisca.com>
2014-07-12 10:41:13 +0530

    alien number has been done

--- AlienNumbers.cs
+++ AlienNumbers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Codejam
 {
@@ -8,10 +9,14 @@ namespace Codejam
     {
         public string getTargetNumber(string alien_number, string source_language, string target_language)
         {
+            FindAlienNumber source = new FindAlienNumber() { languagestring = source_language };
+            FindAlienNumber target = new FindAlienNumber() { languagestring = target_language };
 
-            
+            int number = source.convertDecimal(alien_number);
 
-            return "";   
+            string targetstring = target.getTargetString(number);
+
+            return targetstring;   
             
         }
     
@@ -30,5 +35,59 @@ namespace Codejam
             } while (input != "-1");
         }
         #endregion
+    }
+
+    public class FindAlienNumber
+    { 
+        
+
+       public string languagestring { get; set; }
+
+       public int convertDecimal(string inputstring) 
+       {
+
+           int num = 0;
+           int multiplier = 1;
+
+           inputstring
+                       .Reverse()
+                       .ToList()
+                       .ForEach(x =>
+                       {
+                           num += languagestring.IndexOf(x) * multiplier;
+                           multiplier *= languagestring.Length;
+                       });
+
+           return num;
+       }
+
+       public string getTargetString(int number) {
+
+           int num = number;
+           char[] targetvalue = languagestring.ToCharArray();
+           int div = languagestring.Length;
+           Stack<int> stack = new Stack<int>();
+           int stackvalue = 0;
+           string returnstring ="";
+
+
+
+           do
+           {
+               stackvalue = num % div;
+               num = num / div;
+               stack.Push(stackvalue);
+
+           } while (num != 0);
+
+           foreach( int i in stack)
+           {
+               returnstring = returnstring + targetvalue[i];
+
+           }
+
+           return returnstring;
+
+       }
     }
 }
